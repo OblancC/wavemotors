5cb634cd63efe2338f39667cc70292a3e4f3c991
Octavio <tavinhoblanco@uol.com.br>
2024-05-18 19:10:02 -0300

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { LoginComponent } from './components/layout/login/login.component';
+import { CarroslistComponent } from './components/carros/carroslist/carroslist.component';
+import { CarrosdetailsComponent } from './components/carros/carrosdetails/carrosdetails.component';
+import { MarcaslistComponent } from './components/marcas/marcaslist/marcaslist.component';
+import { MarcasdetailsComponent } from './components/marcas/marcasdetails/marcasdetails.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'principal', pathMatch: 'full'},
+    {path:"principal",component: PrincipalComponent, children:[
+        {path: 'carros', component: CarroslistComponent},
+        {path: 'marcas', component: MarcaslistComponent},
+        {path: 'login', component: LoginComponent},
+    ]},
+
+    {path:'admin', component: PrincipalComponent, children: [
+        {path: 'carros/new', component: CarrosdetailsComponent},
+        {path: 'carros/edit/:id', component: CarrosdetailsComponent},
+        {path:'marcas/new', component: MarcasdetailsComponent},
+        {path:'marcas/edit/:id', component: MarcasdetailsComponent},
+    ]}
+];
---
+++ src/app/components/carros/carrosdetails/carrosdetails.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-carroslist',
+    standalone: true,
+    imports: [],
+    templateUrl: './carrosdetails.component.html',
+    styleUrl: './carrosdetails.component.scss'
+})
+export class CarroslistComponent {
+
+}
