cb65f2fd6f0711e1ac3d329faeb5a7c38b883e5a
Ary de Souza Oliveira Neto <arineto10@hotmail.com>
2024-05-20 18:59:31 -0300

    first commit home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MdbCarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
