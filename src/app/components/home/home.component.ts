d510efc47ba6bb31ea31b0ab816c907571e29b8c
ary <arineto10@hotmail.com>
2024-05-20 21:20:39 -0300

    fixing cards and footer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MdbCarouselModule],
+  imports: [MdbCarouselModule, MdbFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  
 }
